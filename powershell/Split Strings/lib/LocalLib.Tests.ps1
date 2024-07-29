de3b61b8d1d9b219a2886ba35c277c1226432aae
Memor-X <xmemor@gmail.com>
2024-07-29 11:47:38 +1000

    added expanded tests

--- powershell/Split Strings/lib/LocalLib.Tests.ps1
+++ powershell/Split Strings/lib/LocalLib.Tests.ps1
@@ -57,15 +57,52 @@ Describe 'Codewars Tests' {
             $outputBuffer."screen" = @()
         }
 
-    It 'Should pass' {
-        $expected = @("ab","c_")
-		$actual = Split-String("abc")
-		Compare-Object $expected $actual| Should -BeNullOrEmpty
+    Context "Basic Tests" {
+        It "Should pass"{
+            $expected = @("ab","c_")
+            $actual = Split-String("abc")
+            Compare-Object $expected $actual| Should -BeNullOrEmpty
+        }
+        
+        It "Should pass"{
+            $expected = @("ab", "cd", "ef")
+            $actual = Split-String("abcdef")
+            Compare-Object $expected $actual | Should -BeNullOrEmpty
+        }
     }
-
-    It 'Should pass' {
-        $expected = @("ab", "cd", "ef")
-		$actual = Split-String("abcdef")
-		Compare-Object $expected $actual | Should -BeNullOrEmpty
+    
+    Context "Extended Tests" {
+        BeforeEach {
+            $testOne = Split-String("cdabefg")
+            $testTwo = Split-String("abcd")
+        }
+        
+        It "Should not be null"{
+            $testOne| Should -Not -BeNullOrEmpty
+        }
+        
+        It "Should return 4 pairs"{
+            $testOne.Length | Should -Be 4
+        }
+        
+        It "Should have cd in position 0"{
+            $testOne[0] | Should -Be "cd"
+        }
+        
+        It "Should have g_ in position 3"{
+            $testOne[3] | Should -Be "g_"
+        }
+        
+        It "Should not be null"{
+            $testTwo | Should -Not -BeNullOrEmpty
+        }
+        
+        It "Should return 2 pairs"{
+            $testTwo.Length | Should -Be 2
+        }
+        
+        It "Should have cd in position 1"{
+            $testTwo[1] | Should -Be "cd"
+        }
     }
 }
